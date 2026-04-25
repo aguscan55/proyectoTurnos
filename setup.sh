7aeee5aa39dac256602620fa5e0394d2a24b31e7
AbrilCC <abrilelisiricc@gmail.com>
2026-04-25 02:09:07 -0300

    Deleted unnecesarry files

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-python -m pip install -r requirements.txt
-
-#Creo que esto es para levantar el proyecto con django, 
-#despues vemos qué necesitamos que sea similar, para poner aca
-#django-admin startproject slackbot
-#cd slackbot
-#python manage.py startapp events
